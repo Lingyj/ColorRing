ece7224bf0f1598f7f4418928e78579a8731b4b7
Lingyj <lingyj@iiseeuu.com>
2014-08-26 09:54:54 +0800

    Initial Commit

---
+++ ColorRing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ColorRing
+//
+//  Created by lingyj on 14-8-26.
+//  Copyright (c) 2014年 lingyongjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
